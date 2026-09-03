f2a30999c59592a5120108930418387d7afe86cd
gelleson <go.gelleson@gmail.com>
2026-09-03 16:11:11 +1000

    feat: new theme

--- lua/plugins/themes/noir.lua
+++ lua/plugins/themes/noir.lua
@@ -3,4 +3,8 @@ return {
   "tahadx/noir.nvim",
   lazy = false,
   priority = 1000,
+  config = function()
+    -- noir has no config API; its palette table is read at colorscheme load time
+    require("noir.palette").accent = "#84a0c6" -- iceberg blue
+  end,
 }
